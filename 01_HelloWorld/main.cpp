7dff5265a18702e549f9b8e84e6ba3a2f0d282a8
Ahmed Moustafa <ahmed_moustafa@apple.com>
2024-10-16 16:55:32 +0200

    more cleanup

--- 01_HelloWorld/main.cpp
+++ 01_HelloWorld/main.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<climits>
 
 using namespace std;
 
 int main()
 {
+    int i{0};
     cout << "hello world" << endl;
+    cout << "cout value: " << i << ", sizeof: " << sizeof i << ", max value: " << INT32_MAX << ", min value: " << INT32_MIN << endl;
+    
+    string name{"My name is ?"};
+    cout << name << endl;
+
+    const float pi{3.14};
+    cout << "My Pi = " << pi;
+
 }
